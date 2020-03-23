5c55768cd12c8f22579b71212f757efeedb2e866
agzsoftsi <agzsoftsi@gmail.com>
2020-03-23 19:23:36 +0000

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Task 0 - Create a file in temp Using Puppet
+file { '/tmp/holberton':
+    ensure  => file,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
