bd794965040fd6242d2366829c2ea734baa2a547
agzsoftsi <agzsoftsi@gmail.com>
2020-03-24 05:44:23 +0000

    Fix Error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 # Task2 - Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'Killprocess':
-  command => 'pkill killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+exec { 'pkill killmenow':
+path => '/usr/bin'
 }
