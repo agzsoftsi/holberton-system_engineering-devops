e2864a40fd31f0ce0e06bedc2c626d54c2959b1d
agzsoftsi <agzsoftsi@gmail.com>
2020-03-24 11:35:16 +0000

    New update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Kill an active process with pkill command:
-    exec {'Kill process':
-      command => 'pkill -f ./killmenow',
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-    }
