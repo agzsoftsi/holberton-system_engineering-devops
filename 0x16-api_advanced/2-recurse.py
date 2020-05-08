507102edfa7b941e3b5a2b136a08168732fbab8d
agzsoftsi <agzsoftsi@gmail.com>
2020-05-08 00:03:03 +0000

    Task 2 fix pep8 Done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Write a recursive function that queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit. If no 
+containing the titles of all hot articles for a given subreddit. If no
 results are found for the given subreddit, the function should return None.
 """
 import requests
